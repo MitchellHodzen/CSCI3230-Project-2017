a9f9139ee61a9ec57e6eb46fb5c1e092b0f5151b
md05062 <md05062@georgiasouthern.edu>
2017-03-20 12:55:34 -0400

    Opened repository via "Open from Source Control" in Visual Studios

---
+++ Test_VS_Sync/Test_VS_Sync/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_VS_Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /***********************************
+             * If seen: You can                *
+             * "Open from Source Control"      *
+             * in Visual Studios and directly  *
+             * open the repository,            *
+             * push and pull                   *                 
+             ***********************************/
+        }
+    }
+}
