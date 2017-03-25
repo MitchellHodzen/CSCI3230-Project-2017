eeaf20543bf65cbf12d62d403c5056de263ffb78
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2017-03-25 17:51:37 -0400

    Restructured the repository Added folders for each section:         1: Document cleaning, for the document cleaning code         2: Document storage, for the document vector and word frequency code         3: Indexing, for the inverted index code In each document is a readme file where we can do a write up of the code we wrote for that section.
    Wrote code for reading the clean document and generating a document vector, which contains information about what words appear in the document and the frequency of those words.

---
+++ DataStructuresProject2017/DataStructuresProject2017/DocumentStorage/SparseVector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresProject2017.DocumentStorage
+{
+    /*A sparse vector which contains information about the words that appear in the document and their frequency
+    This information is held in a multidimentional array where the first index position represents a word
+    which appears in the document and the second index position represents the frequency of that word in the document.
+    It is a sparse vector because it only contains information about which words are present in a particular document,
+    all other words not present in that document are understood to have a frequency of 0.*/
+
+    public class SparseVector
+    {
+        //Internal array which contains all word frequency information about the document
+        //internalArray[Word Index][Word Freqency]
+        private int[][] internalArray;
+
+        public SparseVector(Dictionary<int, int> docInput)
+        {
+            GenerateVector(docInput);
+        }
+        private void GenerateVector(Dictionary<int, int> docInput)
+        {
+            //Using the word frequency dictionary the internalArray is populated with words and their frequency
+            internalArray = new int[docInput.Count()][];
+            for (int i = 0; i < internalArray.Length; i++)
+            {
+                internalArray[i] = new int[2];
+                internalArray[i][0] = docInput.ElementAt(i).Key;
+                internalArray[i][1] = docInput.ElementAt(i).Value;
+            }
+        }
+
+        public int[] GetNonZeroIndexPositions()
+        {
+            //Returns an array which holds the values in the first index position of the internal array
+            int[] termArray = new int[internalArray.Length];
+            for (int i = 0; i < termArray.Length; i++)
+            {
+                termArray[i] = internalArray[i][0];
+            }
+            return termArray;
+        }
+
+        public void PrintVector()
+        {
+            //Quick way to print out the vector, for testing
+            for (int i = 0; i < internalArray.Length; i++)
+            {
+                Console.WriteLine(internalArray[i][0] + " " + internalArray[i][1]);
+            }
+        }
+    }
+}
