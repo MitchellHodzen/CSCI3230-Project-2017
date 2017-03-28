3e28884dff058bf14f4de4fff4721131161990f4
md05062 <md05062@georgiasouthern.edu>
2017-03-28 18:00:45 -0400

    Update

--- DataStructuresProject2017/TDSBackend/Indexing/Index.cs
+++ DataStructuresProject2017/TDSBackend/Indexing/Index.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TDSBackend.DocumentStorage;
 
 namespace TDSBackend.Indexing {
 
@@ -21,23 +22,24 @@ namespace TDSBackend.Indexing {
         }
 
         //Method to populate the index | O(n*m)
-        private void populateIndex(List<int> list) {
+        private void populateIndex(List<DocumentVector> list) {
 
             //Loop through list of vectors and populate index
-            //for (int i = 0; i < list.Count; i++) //loop through list index
-            //{
-            //    int[] words = List.ElementAt(i).GetDocumentTerms();
-            //    for (int j = 0; j < words.Count; j++) //loop through array of words
-            //    {
-            //        if (index.ContainsKey(j))
-            //        {
-            //            index[j].Add(i);
-            //        } else
-            //        {
-            //            index.Add(j,new List<int>() { i });
-            //        }
-            //    }
-            //}
+            for (int i = 0; i < list.Count; i++) //loop through list index
+            {
+                int[] words = list.ElementAt(i).GetDocumentTerms();
+                for (int j = 0; j < words.Length; j++) //loop through array of words
+                {
+                    if (index.ContainsKey(j))
+                    {
+                        index[j].Add(i);
+                    }
+                    else
+                    {
+                        index.Add(j, new List<int>() { i });
+                    }
+                }
+            }
         }//end of populateIndex
 
         //Method to get values per key
