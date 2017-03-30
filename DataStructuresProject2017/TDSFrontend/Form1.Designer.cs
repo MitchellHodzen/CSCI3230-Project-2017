dd76fca5fac320c337efd6c0a91bb159b391a1a0
md05062 <md05062@georgiasouthern.edu>
2017-03-30 15:37:40 -0400

    GUI update and getting input

--- DataStructuresProject2017/TDSFrontend/Form1.Designer.cs
+++ DataStructuresProject2017/TDSFrontend/Form1.Designer.cs
@@ -28,34 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.retrieveInput = new System.Windows.Forms.Button();
+            this.inputText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // retrieveInput
             // 
-            this.button1.Location = new System.Drawing.Point(448, 143);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.retrieveInput.Location = new System.Drawing.Point(588, 100);
+            this.retrieveInput.Name = "retrieveInput";
+            this.retrieveInput.Size = new System.Drawing.Size(75, 23);
+            this.retrieveInput.TabIndex = 0;
+            this.retrieveInput.Text = "Search";
+            this.retrieveInput.UseVisualStyleBackColor = true;
+            this.retrieveInput.Click += new System.EventHandler(this.Search_Click);
             // 
-            // textBox1
+            // inputText
             // 
-            this.textBox1.Location = new System.Drawing.Point(333, 117);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(300, 20);
-            this.textBox1.TabIndex = 1;
+            this.inputText.Location = new System.Drawing.Point(282, 102);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(300, 20);
+            this.inputText.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(960, 620);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.retrieveInput);
             this.Name = "Form1";
             this.Text = "Text Document Search";
             this.ResumeLayout(false);
@@ -65,8 +65,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button retrieveInput;
+        private System.Windows.Forms.TextBox inputText;
     }
 }
 
--- DataStructuresProject2017/TDSFrontend/Form1.cs
+++ DataStructuresProject2017/TDSFrontend/Form1.cs
@@ -17,9 +17,9 @@ namespace TDSFrontend
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Search_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show(this.inputText.Text);
         }
     }
 }
