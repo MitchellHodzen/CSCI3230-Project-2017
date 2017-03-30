96360eb62468833bcc6e7c056407fec8ac730136
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2017-03-29 22:07:00 -0400

    Implemented Dot Product of Sparse Vectors New SparseVector static method DotProduct which calculates the dot product of two SparseVectors. Will be used to calculate document similarity.

--- DataStructuresProject2017/TDSBackend/DocumentStorage/SparseVector.cs
+++ DataStructuresProject2017/TDSBackend/DocumentStorage/SparseVector.cs
@@ -7,13 +7,27 @@ using System.Threading.Tasks;
 namespace TDSBackend.DocumentStorage
 {
     /*A sparse vector which contains information about the words that appear in the document and their frequency
-    This information is held in a multidimentional array where the first index position represents a word
-    which appears in the document and the second index position represents the frequency of that word in the document.
+    This information is held in a map where the key represents a word which appears in the document and the value
+    represents the frequency of that word in the document.
     It is a sparse vector because it only contains information about which words are present in a particular document,
     all other words not present in that document are understood to have a frequency of 0.*/
 
     public class SparseVector
     {
+        public static double DotProduct(SparseVector v1, SparseVector v2)
+        {
+            //Calculates the dot product of two vectors v1 and v2
+            int[] indexes = v1.GetNonZeroIndexPositions();
+            double temp = 0;
+            double dotProduct = 0;
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                temp = v1.GetValueAtIndex(indexes[i]) * v2.GetValueAtIndex(indexes[i]);
+                dotProduct += temp;
+            }
+            return dotProduct;
+        }
+
         //Internal array which contains all word frequency information about the document
         //internalMap<Word Index, Word Freqency>
         private Dictionary<int, int> internalMap = new Dictionary<int, int>();
@@ -42,6 +56,17 @@ namespace TDSBackend.DocumentStorage
                 //If the index does not already have a value, insert that index with a value of the passed in value
                 internalMap.Add(indexPosition, value);
             }
+        }
+
+        public int GetValueAtIndex(int index)
+        {
+            //If the vector contains a value at that index, return the value. If not return 0
+            int value = 0;
+            if (internalMap.TryGetValue(index, out value))
+            {
+                return value;
+            }
+            return value;
         }
 
         public void PrintVector()
--- DataStructuresProject2017/TDSBackend/Program.cs
+++ DataStructuresProject2017/TDSBackend/Program.cs
@@ -12,7 +12,17 @@ namespace TDSBackend
         static void Main(string[] args)
         {
             //PUT TEST CODE HERE
+            //Test dot product
+            SparseVector v1 = new SparseVector();
+            v1.AddElement(0, 1);
+            v1.AddElement(2, 3);
+            SparseVector v2 = new SparseVector();
+            v2.AddElement(0, 5);
+            v2.AddElement(1, 3);
+            Console.WriteLine("The dot product of v1 and v2 is: " + SparseVector.DotProduct(v1, v2));
 
+
+            //Test the document vector generator
             string input = "oh baby a tripple baby baby tripple oh oh oh oh";
             DocumentVector test = DocumentVectorGenerator.GenerateDocumentVector(input);
             test.Print();
