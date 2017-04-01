61d8079191b187a45ef0a47e6508536dcfa7ca39
md05062 <md05062@georgiasouthern.edu>
2017-04-01 14:53:57 -0400

    Updated populate method and added a new populate method with a better run time

--- DataStructuresProject2017/TDSBackend/Indexing/Index.cs
+++ DataStructuresProject2017/TDSBackend/Indexing/Index.cs
@@ -11,7 +11,7 @@ namespace TDSBackend.Indexing {
          *  This index class will create an dictionary of words and corresponding    *
          *  documents associated with that word. This will allow for fast retrevial  *
          *  of documents when a word is searched.                                    *
-        ******************************************************************************/
+         *****************************************************************************/
 
     public class Index {
         
@@ -21,13 +21,15 @@ namespace TDSBackend.Indexing {
             index = new Dictionary<int, List<DocumentVector>>();
         }
 
-        //Method to populate the index | O(nk) for n documents with max length k
+        /** Method to populate the index given a list| O(nk) for n documents with max length k **/
         public void populateIndex(List<DocumentVector> list) {
 
-            //loop through list of vectors and populate index
+            //loop through the list
             for (int i = 0; i < list.Count; i++)
             {
+                //Store the words in the vector
                 int[] words = list.ElementAt(i).GetDocumentTerms();
+
                 //loop through array of words
                 for (int j = 0; j < words.Length; j++)
                 {
@@ -46,9 +48,33 @@ namespace TDSBackend.Indexing {
             }
         }//end of populateIndex
 
-        //Method to get values per key
+        /** Method to get values per key **/
         public List<DocumentVector> getDocuments(int key) {
             return index[key];
         }//end of getDocuments
+
+/********************************************************************************************/
+
+        /** Method to populate the index given a DocumentVector | **/
+        public void indexPopulate(DocumentVector vector) {
+
+            //get the words from the vector
+            int[] words = vector.GetDocumentTerms();
+
+            for (int j = 0; j < words.Length; j++)
+            {
+                //if key is already in the index
+                if (index.ContainsKey(words[j]))
+                {
+                    //add the vector to the key
+                    index[words[j]].Add(vector);
+                }
+                else
+                {
+                    //if the key is not in the index, add both the key and vector
+                    index.Add(words[j], new List<DocumentVector>() { vector });
+                }
+            }
+        }//end of indexPopulate method
     }
 }//end of Index.cs
